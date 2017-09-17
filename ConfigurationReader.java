ea86bf4a9ca502ab4c476470082f5161af8c9c33
aungphyo88 <aphyo@mail.sfsu.edu>
2017-09-17 13:06:52 -0700

    ConfigurationReader class implement

--- ConfigurationReader.java
+++ ConfigurationReader.java
@@ -12,28 +12,46 @@ package javawebserver;
 import java.util.*;
 import java.io.*;
 
-public class ConfigurationReader {
+public class ConfigurationReader{
       
-      String fname;
-      
+      static String fname = "http.conf";
+      private static BufferedReader bf = null;
+     
       public ConfigurationReader(String fileName){
            fname = fileName;
       }
       
-      public boolean hasMoreLine(){
-          
-           return false; 
+      public boolean hasMoreLine() throws IOException{
+           boolean line = false;
+           if(bf.readLine() != null){
+               line = true;
+           }
+           else{
+               line = false;
+           }
+           return line; 
       }
       
-      public String nextLine(){
-           
-           return "";
+      public String nextLine() throws IOException{
+           String s = null;
+           while(bf.readLine() != null){
+               s = bf.readLine();
+           }
+           return s;
       }
       
-      public void load(){
-          
+      public void load() throws IOException{
+           while(bf.readLine() != null){
+               System.out.println(bf.readLine());
+           }
       }
       public static void main(String[]args){
+          
+        try {
+             bf = new BufferedReader(new FileReader(fname));
+        } catch (FileNotFoundException ex) {
+             ex.getMessage();
+        }
           
       }
       
