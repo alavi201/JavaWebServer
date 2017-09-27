0b0e9178c7c99560c2f3a29c9c756866e3fb9a9b
Ali Alavi <ali.alavi201@gmail.com>
2017-09-26 20:43:05 -0700

    Delete FourHundredAndThree.java

--- FourHundredAndThree.java
+++
@@ -1,9 +0,0 @@
-
-public class FourHundredAndThree extends Response{
-
-    FourHundredAndThree(Resource resource){
-        super(resource);
-        this.code = 403;
-        this.reasonPhrase = "Forbidden";
-    }
-}
