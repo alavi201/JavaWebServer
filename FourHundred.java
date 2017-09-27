425ecc804a96abd98cab369cc4348045940bf452
Ali Alavi <ali.alavi201@gmail.com>
2017-09-26 20:41:51 -0700

    Delete FourHundred.java

--- FourHundred.java
+++
@@ -1,9 +0,0 @@
-
-public class FourHundred extends Response{
-
-    FourHundred(Resource resource){
-        super(resource);
-        this.code = 400;
-        this.reasonPhrase = "Bad Request";
-    }
-}
