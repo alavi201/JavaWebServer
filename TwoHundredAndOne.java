da637ad401979d907630be8bb8352d549d0abd15
Ali Alavi <ali.alavi201@gmail.com>
2017-09-26 20:44:44 -0700

    Delete TwoHundredAndOne.java

--- TwoHundredAndOne.java
+++
@@ -1,9 +0,0 @@
-
-public class TwoHundredAndOne extends Response{
-
-    TwoHundredAndOne(Resource resource){
-        super(resource);
-        this.code = 201;
-        this.reasonPhrase = "File created";
-    }
-}
