3743f2ebe551edafa5b009776fa346dfb1d59b4a
AP020897 <aphyo@mail.sfsu.edu>
2017-09-26 20:45:29 -0700

    Delete TwoHundredAndFour.java

--- TwoHundredAndFour.java
+++
@@ -1,9 +0,0 @@
-
-public class TwoHundredAndFour extends Response{
-
-    TwoHundredAndFour(Resource resource){
-        super(resource);
-        this.code = 204;
-        this.reasonPhrase = "File deleted";
-    }
-}
