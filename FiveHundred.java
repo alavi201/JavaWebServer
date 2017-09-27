246f3d50f49883b09bb16e74a0eff92bd3211049
Ali Alavi <ali.alavi201@gmail.com>
2017-09-26 20:40:44 -0700

    Delete FiveHundred.java

--- FiveHundred.java
+++
@@ -1,9 +0,0 @@
-
-public class FiveHundred extends Response{
-
-    FiveHundred(Resource resource){
-        super(resource);
-        this.code = 500;
-        this.reasonPhrase = "Internal Server Error";
-    }
-}
