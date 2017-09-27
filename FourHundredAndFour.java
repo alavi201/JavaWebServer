09bf3a3c5feb1eb17be40d343ebdaac4cc390757
Ali Alavi <ali.alavi201@gmail.com>
2017-09-26 20:42:42 -0700

    Delete FourHundredAndFour.java

--- FourHundredAndFour.java
+++
@@ -1,9 +0,0 @@
-
-public class FourHundredAndFour extends Response{
-
-    FourHundredAndFour(Resource resource){
-        super(resource);
-        this.code = 404;
-        this.reasonPhrase = "File not found";
-    }
-}
